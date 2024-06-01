bcde710594763528e37258d9f8d17b484f73d0c2
NishargPatel4 <npatel18@flemingcollege.ca>
2024-05-31 21:57:42 -0400

    Revert "Modified both script files"
    This reverts commit 6f9f1692995932b014f4cd34f051e057b67d3884.

--- script1.py
+++ script1.py
@@ -1,2 +1,3 @@
 #print random comment 
 print("Complex is better than complicated.")
+print("Sparse is better than dense.")
--- script2.py
+++ script2.py
@@ -1,3 +1,2 @@
-#print lines from Zen of Python
 print("Beautiful is better han ugy.")
 print("Simple is better than complex")
